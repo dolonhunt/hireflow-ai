803acd07ee6d486a4f653669a936e4262492ce70
Dolon Hunt <95109852+dolonhunt@users.noreply.github.com>
2026-04-22 18:22:45 +0600

    Add files via upload

---
+++ src/utils/supabase/client.ts
@@ -0,0 +1,7 @@
+import { createBrowserClient as supabaseBrowserClient } from "@supabase/ssr";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const createClient = () =>
+  supabaseBrowserClient(supabaseUrl!, supabaseKey!);
